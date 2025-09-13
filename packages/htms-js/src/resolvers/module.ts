6ce9b7e2e2e39918d2ca24772b8347a52a45e9c0
skarab42 <skarab@bluewin.ch>
2025-09-13 09:52:09 +0200

    feat: cache module path resolution

--- packages/htms-js/src/resolvers/module.ts
+++ packages/htms-js/src/resolvers/module.ts
@@ -5,14 +5,19 @@ import url from 'node:url';
 
 import type { Resolver, ResolveTask, Task, TaskInfo } from '../stream/index.js';
 
-function resolveModule(basePath: string, specifier: string) {
+interface ResolvedModule {
+  url: string;
+  path: string;
+}
+
+function resolveModule(basePath: string, specifier: string): ResolvedModule {
   try {
     const require = createRequire(import.meta.url);
     const modulePath = path.resolve(basePath, specifier);
     const resolvedModulePath = require.resolve(modulePath);
     const moduleUrl = url.pathToFileURL(resolvedModulePath).href;
 
-    return { moduleUrl, resolvedModulePath };
+    return { url: moduleUrl, path: resolvedModulePath };
   } catch (error) {
     throw new Error(`Module not found '${specifier}' at '${basePath}'`, { cause: error });
   }
@@ -27,6 +32,7 @@ export class ModuleResolver implements Resolver {
   readonly #basePath: string;
   readonly #specifier: string;
   readonly #cacheModule: boolean;
+  readonly #resolvedModules = new Map<string, ResolvedModule>();
 
   constructor(specifier: string, options?: ModuleResolverOptions) {
     const { basePath = process.cwd(), cacheModule = true } = options ?? {};
@@ -34,6 +40,18 @@ export class ModuleResolver implements Resolver {
     this.#basePath = basePath;
     this.#specifier = specifier;
     this.#cacheModule = cacheModule;
+  }
+
+  #resolveModule(specifier: string): ResolvedModule {
+    let resolvedModule = this.#resolvedModules.get(specifier);
+
+    if (!resolvedModule) {
+      resolvedModule = resolveModule(this.#basePath, specifier);
+
+      this.#resolvedModules.set(specifier, resolvedModule);
+    }
+
+    return resolvedModule;
   }
 
   async #importModule(moduleUrl: string): Promise<Record<string, ResolveTask>> {
@@ -44,14 +62,13 @@ export class ModuleResolver implements Resolver {
 
   async resolve(info: TaskInfo, specifier?: string | undefined): Promise<Task> {
     try {
-      // TODO: cache module path resolution
       const moduleSpecifier = specifier ?? this.#specifier;
-      const { resolvedModulePath, moduleUrl } = resolveModule(this.#basePath, moduleSpecifier);
-      const taskModule = await this.#importModule(moduleUrl);
+      const resolvedModule = this.#resolveModule(moduleSpecifier);
+      const taskModule = await this.#importModule(resolvedModule.url);
       const task = taskModule[info.name];
 
       if (typeof task !== 'function') {
-        const error = new TypeError(`Task function '${info.name}' not found in '${resolvedModulePath}'`);
+        const error = new TypeError(`Task function '${info.name}' not found in '${resolvedModule.path}'`);
 
         return () => Promise.reject(error);
       }
